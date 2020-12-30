908c9bef483bfcc7a696aa1ea4d0892edf936785
kulithian <76757531+kulithian@users.noreply.github.com>
2020-12-30 09:49:51 -0600

    Update static.mqtt.dhtsensor.py
    Rearranged chunks of code and removed some sleeps that likely caused MQTT to timeout randomly. Far more stable

--- static.mqtt.dhtsensor.py
+++ static.mqtt.dhtsensor.py
@@ -9,15 +9,6 @@ import datetime
 import paho.mqtt.client as mqtt
 import Adafruit_DHT
 
-
-#==================================================================================================================================================
-#Usage
-#python mqtt.channel.py <temp topic> <humidity topic> <gpio pin> <optional update frequency>
-# eg python mqtt.channel.py 'cupboard/temperature1' 'cupboard/humidity1' 4
-# will start an instance using 'cupboard/temperature1' as the temperature topic, and using gpio port 4 to talk to a DHT22 sensor
-# it will use the default update time of 300 secons.
-#==================================================================================================================================================
-
 # Type of sensor, can be Adafruit_DHT.DHT11, Adafruit_DHT.DHT22, or Adafruit_DHT.AM2302.
 DHT_TYPE = Adafruit_DHT.DHT22
 
@@ -31,7 +22,7 @@ DHT_PIN4  = 6
 #if (len(sys.argv) < 2):
 #   raise  ValueError('Input arguments of mqtt channel temperature humidity not passed')
 
-MOSQUITTO_HOST = '192.168.1.15' # Example Openhab Server IP
+MOSQUITTO_HOST = '192.168.1.15' # Openhab IP
 MOSQUITTO_PORT = 1883
 MOSQUITTO_TEMP_MSG = str('turtle/temp1') # Old channel name in here
 MOSQUITTO_HUMI_MSG = str('turtle/humidity1') # Old channel name now passed by argument
@@ -55,12 +46,7 @@ MOSQUITTO_LWT_TOPIC = MOSQUITTO_BASE_TOPIC[0] + '/LWT' #Create the last-will-and
 #print('Mosquitto LWT MSG {0}'.format(MOSQUITTO_LWT_TOPIC))
 
 # How long to wait (in seconds) between measurements.
-#print "Args length: " + str(len(sys.argv))
 FREQUENCY_SECONDS      = 120
-
-#if (len(sys.argv) > 7):
-#	FREQUENCY_SECONDS = float(sys.argv[7])
-
 
 print('Logging sensor measurements to {0} every {1} seconds.'.format('MQTT', FREQUENCY_SECONDS))
 print('Press Ctrl-C to quit.')
@@ -74,30 +60,25 @@ try:
     while True:
         # Attempt to get sensor reading.
         humidity, temp1c = Adafruit_DHT.read(DHT_TYPE, DHT_PIN)
-        time.sleep(1)
+        time.sleep(2)
         print('Temperature 1: {0:0.2f} C'.format(temp1c))
         humidity2, temp2c = Adafruit_DHT.read(DHT_TYPE, DHT_PIN2) # Second Sensor
-        time.sleep(1)
+        time.sleep(2)
         print('Temperature 2: {0:0.2f} C'.format(temp2c))
         humidity3, temp3c = Adafruit_DHT.read(DHT_TYPE, DHT_PIN3) # Third Sensor
-        time.sleep(1)
+        time.sleep(2)
         print('Temperature 3: {0:0.2f} C'.format(temp3c))
         humidity4, temp4c = Adafruit_DHT.read(DHT_TYPE, DHT_PIN4) # Fourth Sensor
         print('Temperature 4: {0:0.2f} C'.format(temp4c))
-        # Moved to Try logic
-        #temp = (temp1c * 9/5) + 32
-        #temp2 = (temp2c * 9/5) + 32
-        #temp3 = (temp3c * 9/5) + 32
-        #temp4 = (temp4c * 9/5) + 32
-
+        time.sleep(2)
         # Skip to the next reading if a valid measurement couldn't be taken.
         # This might happen if the CPU is under a lot of load and the sensor
         # can't be reliably read (timing is critical to read the sensor).
         if humidity is None or temp1c is None or humidity2 is None or temp2c is None or humidity3 is None or temp3c is None or humidity4 is None or temp4c is None: # + Second Sensor
-           time.sleep(4)
+           time.sleep(2)
            continue
 
-        currentdate = time.strftime('%Y-%m-%d %H:%M:%S')
+        #currentdate = time.strftime('%Y-%m-%d %H:%M:%S')
         #print('Date Time:   {0}'.format(currentdate))
         #print('Temperature 1: {0:0.2f} C'.format(temp1c))
         #print('Temperature 1F: {0:0.2f} F'.format(temp))
@@ -117,43 +98,38 @@ try:
             ## new version
             # SENSOR 1
             if humidity is not None and temp1c is not None:
-                temp = (temp1c * 9/5) + 32
-                (result1,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG,temp)
-                time.sleep(1)
                 (result2,mid) = mqttc.publish(MOSQUITTO_HUMI_MSG,humidity)
+                temp = (temp1c * 9/5) + 32
                 time.sleep(1)
+                (result1,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG,temp)
             else:
                 print("Failed to retrieve data from sensor 1")
             # SENSOR 2
             if humidity2 is not None and temp2c is not None:
-                temp2 = (temp2c * 9/5) + 32
-                (result3,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG2,temp2)
-                time.sleep(1)
                 (result4,mid) = mqttc.publish(MOSQUITTO_HUMI_MSG2,humidity2)
+                temp2 = (temp2c * 9/5) + 32
                 time.sleep(1)
+                (result3,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG2,temp2)
             else:
                 print("Failed to retrieve data from sensor 2")
             # SENSOR 3
-            if humidity is not None and temp1c is not None:
-                temp3 = (temp3c * 9/5) + 32
-                (result5,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG3,temp3)
-                time.sleep(1)
+            if humidity3 is not None and temp3c is not None:
                 (result6,mid) = mqttc.publish(MOSQUITTO_HUMI_MSG3,humidity3)
+                temp3 = (temp3c * 9/5) + 32
                 time.sleep(1)
+                (result5,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG3,temp3)
             else:
                 print("Failed to retrieve data from sensor 3")
             # SENSOR 4
-            if humidity is not None and temp1c is not None:
-                temp4 = (temp4c * 9/5) + 32
-                (result7,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG4,temp4)
-                time.sleep(1)
+            if humidity4 is not None and temp4c is not None:
                 (result8,mid) = mqttc.publish(MOSQUITTO_HUMI_MSG4,humidity4)
+                temp4 = (temp4c * 9/5) + 32
                 time.sleep(1)
+                (result7,mid) = mqttc.publish(MOSQUITTO_TEMP_MSG4,temp4)
             else:
                 print("Failed to retrieve data from sensor 4")
-            # End if
+            # Check results
             print('MQTT Updated result {0}, {1}, {2}, {3}, {4}, {5}, {6}, {7} '.format(result1,result2,result3,result4,result5,result6,result7,result8))
-            #print 'MQTT Updated result {0} and {1}'.format(result3,result4)
             if result1 == 1 or result2 == 1 or result3 == 1 or result4 == 1 or result5 == 1 or result6 == 1 or result7 == 1 or result8 == 1:
                 raise ValueError('Result for one message was not 0')
 
@@ -166,6 +142,8 @@ try:
         # Wait 30 seconds before continuing
         print('Wrote a message to MQTT broker')
         time.sleep(FREQUENCY_SECONDS)
+        #print('restarting')
 
 except Exception as e:
     print('Error connecting to the MQTT server: {0}'.format(e))
+
